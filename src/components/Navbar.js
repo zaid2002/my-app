79d31bd03517f2d3f2b9b90426e2a36984459a4e
Zaid Shariff <zaidshamsahmed@gmail.com>
2024-08-17 15:27:34 +0530

    Navbar.js
    update till ep20

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,11 +19,24 @@ export default function Navbar(props) {
             </li> */}
           </ul>
 
-          <form className="d-flex mx-3">
+                          {/* SEARCH INPUT  */}
+          {/* <form className="d-flex mx-3">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-success" type="submit">Search</button>
-          </form>
+          </form> */}
 
+                    {/* This is for CHANGE BG-COLOR MODE*/}
+          {/* <div className="d-flex">
+            <div className="bg-primary rounded mx-2" onClick={()=>{ props.toggleMode('primary')}} style={{height: '30px', width : '30px', cursor: 'pointer' }}></div>
+
+            <div className="bg-warning rounded mx-2" onClick={()=>{ props.toggleMode('warning')}} style={{height: '30px', width : '30px', cursor: 'pointer' }}></div>
+
+            <div className="bg-success rounded mx-2" onClick={()=>{ props.toggleMode('success')}} style={{height: '30px', width : '30px', cursor: 'pointer' }}></div>
+            
+            <div className="bg-danger rounded mx-2" onClick={()=>{ props.toggleMode('danger')}} style={{height: '30px', width : '30px', cursor: 'pointer' }}></div>
+          </div> */}
+
+                                {/* TOGGLE MODE */}
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>        {/*Javascript*/}
 
             <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
